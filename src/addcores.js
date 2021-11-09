75f5493e37d9b3c6eda113a89c7b28b13013489e
Oluwafemi <femikola23@gmail.com>
2021-11-09 19:01:40 +0400

    Add Styles

--- src/addcores.js
+++ src/addcores.js
@@ -4,8 +4,10 @@ const addScoresToList = (score) => {
     const listItem = document.createElement('li');
   
     listItem.innerHTML = `
+        <div class="disflex list-item">
         <p>${score.userName}:</p>
         <p>${score.score}</p>
+        </div>
         `;
   
     list.appendChild(listItem);
